4651db81ebbb61edd4824e3d124475e1423ba162
jiten1711 <jitensutarbrjn2000@gmail.com>
2023-06-30 16:34:16 +0530

    deployement configuration foor AWS

--- src/utils.py
+++ src/utils.py
@@ -54,8 +54,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         raise CustomException(e, sys)
     
 def load_object(file_path):
+    print(file_path)
     try:
         with open(file_path, "rb") as file_obj:
+            print("returning file loading")
             return pickle.load(file_obj)
 
     except Exception as e:
